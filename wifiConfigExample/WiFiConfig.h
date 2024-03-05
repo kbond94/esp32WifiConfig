2dc74eb6f3da9df6be455840fe49e86abe9e1d76
kbond94 <keithbond94@gmail.com>
2024-03-05 01:16:15 +0000

    third commit

--- wifiConfigExample/WiFiConfig.h
+++ wifiConfigExample/WiFiConfig.h
@@ -1,9 +1,18 @@
 #ifndef WiFiConfig_h
 #define WiFiConfig_h
 #include <Arduino.h>
+#include <WiFi.h>
+#include "esp_wpa2.h"
 
 class WiFiConfig{
   public:
+    //config[][0] = ssid, [][1] = password, [][2] = username (normally null), [3] = type (0 = standard, 1 = eduroam) 
+    
+    const char* configs[5][4];
+    void setConfig (int n);
+
+    WiFiConfig();
+
 
 }
 
